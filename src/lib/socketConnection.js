5db64a359dbd416007c270369e97ccd3b379ef53
dannydongin <47995530+dannydongin@users.noreply.github.com>
2020-08-09 17:40:00 +0900

    modify 채팅 로그 저장

--- src/lib/socketConnection.js
+++ src/lib/socketConnection.js
@@ -10,7 +10,10 @@ module.exports = function(io) {
         socket.name = name
 
         // 나중에 전송할 챗로그 엔트리 만들기
-        CHATLOG[name] = "";
+        CHATLOG[name] = {};
+        CHATLOG[name].name = name;
+        CHATLOG[name].line = [];
+        
 
         /* 모든 소켓에게 전송 */
         io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
@@ -22,7 +25,7 @@ module.exports = function(io) {
         data.name = socket.name
 
         // 챗로그 해당 이름 앞으로 메세지 추가
-        CHATLOG[socket.name] += data.message;
+        CHATLOG[socket.name].line.push(data.message);
 
         //console.log(data)
 
@@ -35,7 +38,9 @@ module.exports = function(io) {
         console.log(socket.name + '님이 나가셨습니다.')
 
         // 나간 사람의 챗로그 데이터 출력(나중에 전송 및 DB저장으로 수정)
-        console.log([socket.name, CHATLOG[socket.name]]);
+        console.log(CHATLOG[socket.name]);
+        let chatlog = JSON.stringify(CHATLOG[socket.name]);
+        
 
         /* 나가는 사람을 제외한 나머지 유저에게 메시지 전송 */
         socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
