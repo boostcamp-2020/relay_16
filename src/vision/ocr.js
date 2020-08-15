8fae553d42b32ac44cc37767a055c418eccadf5a
2hoyeong <leehodud302@naver.com>
2020-08-15 23:20:00 +0900

    OCR 코드 변경
    refactor : 네이버 OCR API 호출부분 PROMISE로 변경

--- src/vision/ocr.js
+++ src/vision/ocr.js
@@ -1,30 +1,32 @@
 const request = require('request');
 
-exports.callOCRAPI = function (imagedata, callback) {
-    const sendData = {
-        headers: {
-            'Content-Type' : 'application/json',
-            'X-OCR-SECRET' : 'aXFxelVqT3JnSHdtSFpIVkVianlsZGZ5dm9GQ0dUZXM='
-        },
-        url: 'https://5ae4d251b31a4dbfb8f0252852b2d6e5.apigw.ntruss.com/custom/v1/3373/5531832d937a4d5de975b1960a6c5bbc39d4200f4447978ac039fa1bf6e8c85d/general',
-        body: JSON.stringify({
-            "images": [
-                {
-                  "format": "jpg",
-                  "name": "medium",
-                  "data" : imagedata,
-                  "url": null
-                }
-              ],
-              "lang": "ko",
-              "requestId": "string",
-              "resultType": "string",
-              "timestamp": 0,
-              "version": "V1"
-        }),
-    };
-
-    request.post(sendData, (err, res, result) => {
-        callback(res, JSON.parse(result));
+exports.getTextFromImageData = function (imagedata) {
+    return new Promise((resolve, reject) => {
+        const sendData = {
+            headers: {
+                'Content-Type' : 'application/json',
+                'X-OCR-SECRET' : 'aXFxelVqT3JnSHdtSFpIVkVianlsZGZ5dm9GQ0dUZXM='
+            },
+            url: 'https://5ae4d251b31a4dbfb8f0252852b2d6e5.apigw.ntruss.com/custom/v1/3373/5531832d937a4d5de975b1960a6c5bbc39d4200f4447978ac039fa1bf6e8c85d/general',
+            body: JSON.stringify({
+                "images": [
+                    {
+                      "format": "png",
+                      "name": "medium",
+                      "data" : imagedata,
+                      "url": null,
+                    }
+                  ],
+                  "lang": "ko",
+                  "requestId": "string",
+                  "resultType": "string",
+                  "timestamp": 0,
+                  "version": "V1"
+            }),
+        };
+        request.post(sendData, (err, res, result) => {
+            if(err) reject(err);
+            resolve(JSON.parse(result));
+        })
     })
 };
